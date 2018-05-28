e056ec025ff625400cbdd31f7a94a1615a95db4f
bfischl <bradley.fischl@gmail.com>
2018-05-27 23:22:10 -0400

    Multi monitor fix

--- Desktop/.config/polybar/launch.sh
+++ Desktop/.config/polybar/launch.sh
@@ -1,7 +1,14 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m 
+        echo $MONITOR
+        export MONITOR
+        polybar --reload top &
+        polybar --reload bottom &
+    done
+else
 polybar top &
-polybar top2 &
 polybar bottom & 
-polybar bottom2 &
+fi 
